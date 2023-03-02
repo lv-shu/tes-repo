504f725a6434812cf42c390623463a4dd1aa0cd6
Lu Shu <124060764+lv-shu@users.noreply.github.com>
2023-03-02 11:35:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')  
+data = ['mouse','keyboard', 'monitor', 'bottle']
+
+data = [i.upper() for i in data ]
+
+data2 = [True,False,True,False,True,False,True,False,True,False,True,False]
+dataBits = [1 if b == True else 0 for b in data2]
+
+gua = 'HaloNamaGuaRidho'
+gua = ''.join(
+    [i if i.islower() else ' '+ i for i in gua]
+    )
+print(data2)
+print(dataBits)
+print(gua)
